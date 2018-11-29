9674bbcf075f9da3e1840448c908a344476952a9
karolschewe <karol.schewe@gmail.com>
2018-11-29 14:48:33 +0100

    Deleted unnecessary files. Rewrote calculating connections to work on Sensor_list and use all sensors not only first day.

--- class_id_connections.py
+++ class_id_connections.py
@@ -6,18 +6,18 @@ class id_connections:
     longitude = 0.0
     connections = []
 
-    def __init__(self,id,lat,long,sensor_list,max_dist):
-        self.id = id
-        self.latitude = lat
-        self.longitude = long
+    def __init__(self,sensorek,sensor_list,max_dist):
+        self.id = sensorek.id
+        self.latitude = sensorek.latitude
+        self.longitude = sensorek.longitude
         temp_id_list = []
         for i in sensor_list:
-            me = (lat, long)
-            it = (i['latitude'], i['longitude'])
+            me = (sensorek.latitude, sensorek.longitude)
+            it = (i.latitude, i.longitude)
             dist = great_circle(me, it).kilometers
             if dist < max_dist:
-                if self.id != i['id']:
-                    temp_id_list.append(i['id'])
+                if self.id != i.id:
+                    temp_id_list.append(i.id)
 
 
 
--- tworzenie_polaczen.py
+++ tworzenie_polaczen.py
@@ -7,7 +7,27 @@ import matplotlib.pyplot as plt
 startTime = datetime.now() #mierzenie czasu wykonywania programu
 
 
+#dodac procentowo ile jest wiecej
+#czyli dywergencja/srednia w otoczeniu
+#obliczyc średnią dywergencję dla czujnika
+#i znaleźć czujniki ze średnią powyżej średniej
+#robic rozklad srednich dywergencji
+#wykres z tego
+#zrobic mapę z srednich dywergencji (np wykres: x szerokość y długość)
+#pomyslec zeby wyliczone wariancje dla sensora wyrzucac do pliku i potem tylko to wczytywac i na tym liczyc zamiast zawsze od nowa
+# w ogole dzielic sobie program na segmenty
+#korelacje serii czasowych sasiadow
+#korelacje dywergencji sasiadow
+#zrobic model zerowy -- wrzucic jako dane z czujników rozklad jednostajny i policzyc to samo
 
+
+#test importu danych do airly
+test = []
+plik_test = open("airly_sensor_list",'r')
+for line in plik_test:
+    test.append(line.strip())
+print("lista czujnikow do airly")
+print(test)
 
 all_files_in_dir = os.listdir()
 #wyciaganie plikow z danymi -- zalozenie: koncza sie na .txt
@@ -38,22 +58,7 @@ print(json_list[0][123]['history'][0]['measurements']['pm1'])
 from class_id_connections import id_connections
 
 
-#tworzenie sieci polaczen i eksport do pliku
 
-#connections = []
-#plik = open("siec_polaczen","w")
-#for i in json_list[0]:
-#     connections.append(id_connections(i['id'],i['latitude'],i['longitude'],json_list[0],5))
-#
-# for i in connections:
-#     plik.write("id="+str(i.id))
-#     plik.write("\n")
-#     for j in i.connections:
-#         plik.write(str(j))
-#         plik.write("\n")
-# plik.close()
-#
-#
 
 # inicjalizacja klasy sensor
 #import polaczen sensorow z pliku
@@ -61,9 +66,42 @@ from class_sensor import Sensor
 sensor_list = []
 for i in json_list[0]:
     sensor_list.append(Sensor(i['id'],i['latitude'],i['longitude']))
+print("liczba sensorow")
+print(len(sensor_list))
+print("liczba sensorow")
+#sprawdzenie czy nie ma wiecej sensorow w reszcie
+for i in json_list:
+    for j in i:
+        temp_id = j['id']
+        boolean = False
+        for k in sensor_list:
+            if k.id == temp_id:
+                boolean = True
+        if boolean is False:
+            sensor_list.append(Sensor(j['id'], j['latitude'], j['longitude']))
+print("liczba sensorow po sprawdzeniu 4 plikow")
+print(len(sensor_list))
+#wyliczenie polaczen
+
+connections = []
+plik = open("siec_polaczen","w")
+for i in sensor_list:
+    connections.append(id_connections(i,sensor_list,5))
+
+for i in connections:
+    plik.write("id="+str(i.id))
+    plik.write("\n")
+    for j in i.connections:
+        plik.write(str(j))
+        plik.write("\n")
+plik.close()
+
+
+
 for i in sensor_list:
     i.import_connections()
     i.measurements = i.json_to_observations(json_list)
+print("zakonczono tworzenie polaczen")
 # for i in sensor_list:
 #     i.calc_var_pm10(sensor_list)
 print(sensor_list[0].connections)
@@ -87,5 +125,20 @@ print (sensor_list[44].latitude)
 print("long:")
 print(sensor_list[44].longitude)
 print(sensor_list[44].connections)
+puste = 0
+lista_do_airly = []
+plik_sensory = open("airly_sensor_list",'a')
+for i in sensor_list:
+    if len(i.connections) == 0:
+        print(i.id)
+        puste+=1
+    else:
+        print(i.id, file=plik_sensory)
+        lista_do_airly.append(i.id)
 
+print("liczba pustych sensorow")
+print(puste)
+print ("liczba niepustych sensorow")
+print(len(lista_do_airly))
+print()
 print("Program wykonywał się " + str((datetime.now() - startTime).seconds)+ " sekund")
